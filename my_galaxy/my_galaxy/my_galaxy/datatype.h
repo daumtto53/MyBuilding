f04cac853f7076a054e737d433bd64f8bf3433d4
Wooseok Chun <woo1seok@naver.com>
2019-12-07 22:41:06 +0900

    수정본

---
+++ my_galaxy/my_galaxy/my_galaxy/datatype.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdio.h >
+
+typedef struct planet {
+	
+	char* name;
+	int race;
+	char* race_name;
+	int population;
+	int attk_p;
+	int def_p;
+	int fight; //싸우는 상대방
+	int dead;  //
+	int a_or_d; //공격 상태 or 방어 상태
+
+}Planet;
